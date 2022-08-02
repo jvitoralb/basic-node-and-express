1ba7eb149991806fda50559f50be49c460113028
jvitoralb <jvvalb@gmail.com>
2022-08-02 10:59:47 -0300

    use .env

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
+let dotenv = require('dotenv').config()
 let express = require('express');
 let app = express();
 
 console.log('Hello World');
-
-// app.get('/', (req, res) => res.send('Hello Express'));
 
 app.get('/', (req, res) => {
     let absolutePath = `${__dirname}/views/index.html`;
@@ -13,7 +12,12 @@ app.get('/', (req, res) => {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
-    res.json({"message": "Hello json"});
+    const message = {"message": "Hello json"};
+
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        message["message"] = message["message"].toLocaleUpperCase();
+    }
+    return res.json(message);
 });
 
 
