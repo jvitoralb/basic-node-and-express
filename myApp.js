a9e8d340aaed3271c5c1d2ec0b631393887043d3
jvitoralb <jvvalb@gmail.com>
2022-08-02 20:56:26 -0300

    chain middleware func

--- myApp.js
+++ myApp.js
@@ -25,6 +25,13 @@ app.get('/json', (req, res) => {
     res.json(message);
 });
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({"time": req.time});
+});
+
 
 
 
